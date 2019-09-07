acdd6695dba04c2dec4e3f9a0db4b0a7b75b1ebd
Konstantin D <dev.konst@gmail.com>
2019-09-08 01:44:58 +0400

    - added the Carthage support

--- Example/Pods/Target Support Files/Pods-ExpyTableView_Tests/Pods-ExpyTableView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExpyTableView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ExpyTableView_Tests
-@end
---
+++ ExpyTableView/ExpyTableView/ExpyTableView.h
@@ -0,0 +1,19 @@
+//
+//  ExpyTableView.h
+//  ExpyTableView
+//
+//  Created by Konstantin D. on 08/09/2019.
+//  Copyright © 2019 Konstantin D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExpyTableView.
+FOUNDATION_EXPORT double ExpyTableViewVersionNumber;
+
+//! Project version string for ExpyTableView.
+FOUNDATION_EXPORT const unsigned char ExpyTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExpyTableView/PublicHeader.h>
+
+
